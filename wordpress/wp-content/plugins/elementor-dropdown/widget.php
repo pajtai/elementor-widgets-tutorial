c2a0ee3894747133f048efe5217ddf63bce5fc7f
Peter Ajtai <peter.ajtai@soliddigital.com>
2020-01-24 17:13:30 -0800

    Added two simple text controls

--- wordpress/wp-content/plugins/elementor-dropdown/widget.php
+++ wordpress/wp-content/plugins/elementor-dropdown/widget.php
@@ -14,4 +14,37 @@ class Elementor_Dropdown_Widget extends \Elementor\Widget_Base {
 	public function get_icon() { return 'fa fa-caret-down'; }
 
 	public function get_categories() { return [ 'general' ]; }
+
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Options', 'plugin-name' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'widget_value',
+			[
+				'label' => __( 'Option Value', self::$slug ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => __( 'value', self::$slug ),
+				'placeholder' => __( 'Value Attribute', self::$slug ),
+			]
+		);
+
+		$this->add_control(
+			'widget_contents',
+			[
+				'label' => __( 'Option Contents', self::$slug ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => __( 'contents', self::$slug ),
+				'placeholder' => __( 'Option Contents', self::$slug ),
+			]
+		);
+
+		$this->end_controls_section();
+	}
 }
